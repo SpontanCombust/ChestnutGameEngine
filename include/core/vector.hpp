5aa8861460e56db30cba3b3ec1eb6a84b8cb6358
SpontanCombust <cedro_p@o2.pl>
2020-09-16 03:16:22 +0200

    Extended functionality and operator changes - operator methods that had non-void return types  moved outside struct - added functions giving arithmetic functionlity outside of struct -added print method - put the whole thing in 'vector' namespace

--- include/core/vector.hpp
+++ include/core/vector.hpp
@@ -1,33 +1,82 @@
 #pragma once
 
-template< typename T >
-struct Vector2
+#include <string>
+
+namespace vector
 {
-    T x, y;
+    template< typename T >
+    struct Vector2
+    {
+        T x, y;
 
-    Vector2() : x(0), y(0) {}
-    Vector2( T _x, T _y ) : x(_x), y(_y) {} 
+        Vector2() : x(0), y(0) {}
+        Vector2( T _x, T _y ) : x(_x), y(_y) {} 
 
-    T getMagnitude() const;
+        T getMagnitude() const;
 
-    void normalize();
+        void normalize();
 
-    void negate();
-    void operator-();
+        void negate();
 
-    void add( const Vector2& other );
-    void operator+=( const Vector2& other );
-    Vector2 operator+( const Vector2& other ) const;
+        void add( const Vector2& other );
+        void operator+=( const Vector2& other );
 
-    void subtract( const Vector2& other );
-    void operator-=( const Vector2& other );
-    Vector2 operator-( const Vector2& other ) const;
+        void subtract( const Vector2& other );
+        void operator-=( const Vector2& other );
 
-    void multiplyByScalar( const T s );
-    void operator*=( const T s );
+        void multiplyByScalar( const T s );
+        void operator*=( const T s );
 
-    T multiplyDot( const Vector2<T>& other ) const;
-};
+        T multiplyByVecDot( const Vector2<T>& other ) const;
 
-typedef Vector2<float> Vector2f;
-typedef Vector2<int> Vector2i;
+        std::string to_string();
+    };
+
+
+    template< typename T >
+    Vector2<T> getNormalized( const Vector2<T>& v );
+
+
+    template< typename T >
+    Vector2<T> getNegated( const Vector2<T>& v );
+
+    template< typename T >
+    Vector2<T> operator-( const Vector2<T>& v );
+
+
+    template< typename T >
+    Vector2<T> getSum( const Vector2<T>& v1, const Vector2<T>& v2 );
+
+    template< typename T >
+    Vector2<T> operator+( const Vector2<T>& v1, const Vector2<T>& v2 );
+
+
+    template< typename T >
+    Vector2<T> getDifference( const Vector2<T>& v1, const Vector2<T>& v2 );
+
+    template< typename T >
+    Vector2<T> operator-( const Vector2<T>& v1, const Vector2<T>& v2 );
+
+
+    template< typename T >
+    Vector2<T> getScaled( const Vector2<T>& v, T s );
+
+    template< typename T >
+    Vector2<T> operator*( T s, const Vector2<T>& v );
+
+    template< typename T >
+    Vector2<T> operator*( const Vector2<T>& v, T s );
+
+
+    template< typename T >
+    T getDotProduct( const Vector2<T>& v1, const Vector2<T>& v2 );
+
+    template< typename T >
+    T operator*( Vector2<T> v1, Vector2<T> v2 ) = delete; // forbidden due to ambiguity between dot and cross product
+
+
+    // predefined vectors //
+    typedef Vector2<float> Vector2f;
+    typedef Vector2<int> Vector2i;
+
+} // namespace vector
--- src/core/vector.cpp
+++ src/core/vector.cpp
@@ -2,89 +2,180 @@
 
 #include <cmath>
 
-template< typename T >
-T Vector2<T>::getMagnitude() const
+namespace vector
 {
-    return sqrt( x * x + y * y );
-}
-
-template< typename T >
-void Vector2<T>::normalize() 
-{
-    if( x != 0 && y != 0 )
+    template< typename T >
+    T Vector2<T>::getMagnitude() const
     {
-        T mag = getMagnitude();
-        x /= mag;
-        y /= mag;
+        return sqrt( x * x + y * y );
     }
-}
 
-template< typename T >
-void Vector2<T>::negate() 
-{
-    x = -x;
-    y = -y;
-}
 
-template< typename T >
-void Vector2<T>::operator-() 
-{
-    negate();
-}
 
-template< typename T >
-void Vector2<T>::add( const Vector2<T>& other ) 
-{
-    x += other.x;
-    y += other.y;
-}
+    template< typename T >
+    void Vector2<T>::normalize() 
+    {
+        if( x != 0 && y != 0 )
+        {
+            T mag = getMagnitude();
+            x /= mag;
+            y /= mag;
+        }
+    }
 
-template< typename T >
-void Vector2<T>::operator+=( const Vector2<T>& other ) 
-{
-    add(other);
-}
+    template< typename T >
+    Vector2<T> getNormalized( const Vector2<T>& v ) 
+    {
+        Vector2<T> vCopy = v;
+        vCopy.normalize();
+        return vCopy;
+    }
 
-template< typename T >
-Vector2<T> Vector2<T>::operator+( const Vector2<T>& other ) const
-{
-    return Vector2<T>( x + other.x, y + other.y );
-}
 
-template< typename T >
-void Vector2<T>::subtract( const Vector2<T>& other ) 
-{
-    x -= other.x;
-    y -= other.y;
-}
 
-template< typename T >
-void Vector2<T>::operator-=( const Vector2<T>& other ) 
-{
-    subtract(other);
-}
+    template< typename T >
+    void Vector2<T>::negate() 
+    {
+        x = -x;
+        y = -y;
+    }
 
-template< typename T >
-Vector2<T> Vector2<T>::operator-( const Vector2<T>& other ) const
-{
-    return Vector2<T>( x - other.x, y - other.y );
-}
+    template< typename T >
+    Vector2<T> getNegated( const Vector2<T>& v ) 
+    {
+        Vector2<T> vCopy = v;
+        vCopy.negate();
+        return vCopy;
+    }
 
-template< typename T >
-void Vector2<T>::multiplyByScalar( const T s ) 
-{
-    x *= s;
-    y *= s;
-}
+    template< typename T >
+    Vector2<T> operator-( const Vector2<T>& v ) 
+    {
+        Vector2<T> vCopy = v;
+        vCopy.negate();
+        return vCopy;
+    }
 
-template< typename T >
-void Vector2<T>::operator*=( const T s ) 
-{
-    multiplyByScalar(s);
-}
 
-template< typename T >
-T Vector2<T>::multiplyDot( const Vector2<T>& other ) const
-{
-    return x * other.x + y * other.y;
+
+    template< typename T >
+    void Vector2<T>::add( const Vector2<T>& other ) 
+    {
+        x += other.x;
+        y += other.y;
+    }
+
+    template< typename T >
+    void Vector2<T>::operator+=( const Vector2<T>& other ) 
+    {
+        add(other);
+    }
+
+    template< typename T >
+    Vector2<T> getSum( const Vector2<T>& v1, const Vector2<T>& v2 ) 
+    {
+        Vector2<T> v1Copy = v1;
+        v1Copy.add(v2);
+        return v1Copy;
+    }
+
+    template< typename T >
+    Vector2<T> operator+( const Vector2<T>& v1, const Vector2<T>& v2 ) 
+    {
+        Vector2<T> v1Copy = v1;
+        v1Copy.add(v2);
+        return v1Copy;
+    }
+
+
+
+    template< typename T >
+    void Vector2<T>::subtract( const Vector2<T>& other ) 
+    {
+        x -= other.x;
+        y -= other.y;
+    }
+
+    template< typename T >
+    void Vector2<T>::operator-=( const Vector2<T>& other ) 
+    {
+        subtract(other);
+    }
+
+    template< typename T >
+    Vector2<T> getDifference( const Vector2<T>& v1, const Vector2<T>& v2 ) 
+    {
+        Vector2<T> v1Copy = v1;
+        v1Copy.subtract(v2);
+        return v1Copy;
+    }
+
+    template<typename T>
+    Vector2<T> operator-( const Vector2<T>& v1, const Vector2<T>& v2 )
+    {
+        Vector2<T> v1Copy = v1;
+        v1Copy.subtract(v2);
+        return v1Copy;
+    }
+
+
+
+    template< typename T >
+    void Vector2<T>::multiplyByScalar( const T s ) 
+    {
+        x *= s;
+        y *= s;
+    }
+
+    template< typename T >
+    void Vector2<T>::operator*=( const T s ) 
+    {
+        multiplyByScalar(s);
+    }
+
+    template< typename T >
+    Vector2<T> getScaled( const Vector2<T>& v, T s ) 
+    {
+        Vector2<T> vCopy = v;
+        vCopy.multiplyByScalar(s);
+        return vCopy;
+    }
+
+    template< typename T >
+    Vector2<T> operator*( T s, const Vector2<T>& v ) 
+    {
+        Vector2<T> vCopy = v;
+        vCopy.multiplyByScalar(s);
+        return vCopy;
+    }
+
+    template< typename T >
+    Vector2<T> operator*( const Vector2<T>& v, T s ) 
+    {
+        Vector2<T> vCopy = v;
+        vCopy.multiplyByScalar(s);
+        return vCopy;
+    }
+
+
+
+    template< typename T >
+    T Vector2<T>::multiplyByVecDot( const Vector2<T>& other ) const
+    {
+        return x * other.x + y * other.y;
+    }
+
+    template< typename T >
+    T getDotProduct( const Vector2<T>& v1, const Vector2<T>& v2 ) 
+    {
+        return v1.multiplyByVecDot(v2);
+    }
+
+
+
+    template<typename T>
+    std::string Vector2<T>::to_string() 
+    {
+        return "[ " + std::to_string(x) + ", " + std::to_string(y) + " ]";
+    }
 }
