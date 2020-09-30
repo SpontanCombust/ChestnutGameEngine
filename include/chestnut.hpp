7f743a7eeb7b5e98ac3894cd3f54d833cf6f6155
SpontanCombust <cedro_p@o2.pl>
2020-10-01 01:04:48 +0200

    ECS first implementation

--- include/chestnut.hpp
+++ include/chestnut.hpp
@@ -2,11 +2,14 @@
 #define __CHESTNUT_CHESTNUT_H__
 
 #include "engine/core/vector.hpp"
+#include "engine/core/angles.hpp"
 
 #include "engine/debug/debug.hpp"
 
 #include "engine/graphics/texture_resource.hpp"
 #include "engine/graphics/texture.hpp"
+
+#include "engine/ecs/ecs.hpp"
 
 #include "engine/app/render_window.hpp"
 #include "engine/app/engine.hpp"
