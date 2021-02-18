a32357e57cfdb9c13edfae4e0bf3c991d6a0054b
SpontanCombust <cedro_p@o2.pl>
2021-02-19 00:05:19 +0100

    Added 'cancelEvents' function

--- src/engine/ecs/system_bases/event_raising_system.hpp
+++ src/engine/ecs/system_bases/event_raising_system.hpp
@@ -4,6 +4,7 @@
 #include "engine/event_system/event_manager.hpp"
 
 #include <queue>
+#include <functional>
 
 namespace chestnut
 {
@@ -17,7 +18,48 @@ namespace chestnut
         virtual void raiseEvents( CEventManager& eventManagerRef );
         
         virtual ~CEventRaisingSystem() {}
+
+    protected:
+        template< typename EventType >
+        void cancelEvents( std::function< bool ( const EventType& ) > cancelationRule );
     };
+
+
+
+    template< typename EventType >
+    void CEventRaisingSystem::cancelEvents( std::function< bool ( const EventType& ) > cancelationRule ) 
+    {
+        // Queue to push to only events that don't fit cancelation rule
+        std::queue< SEvent* > filteredLocalEventQueue;
+
+        SEvent *event;
+        EventType *castedEvent;
+        while( !m_localEventQueue.empty() )
+        {
+            event = m_localEventQueue.front();
+            castedEvent = dynamic_cast< EventType* >( event );
+            m_localEventQueue.pop();
+            
+            // If event's type is not the one specified for the function, automatically push it to the queue with filtered out events
+            if( !castedEvent )
+            {
+                filteredLocalEventQueue.push( event );
+            }
+            // If event type matches, check the cancelation rule. If it's successful, get rid of the event.
+            else if( cancelationRule( *castedEvent ) )
+            {
+                delete castedEvent;
+            }
+            // ... otherwise push the event to the queue with filtered out events
+            else
+            {
+                filteredLocalEventQueue.push( castedEvent );
+            }
+        }
+
+        // Copy-assign filtered events back to local event queue
+        m_localEventQueue = filteredLocalEventQueue;
+    }
 
 } // namespace chestnut
 
