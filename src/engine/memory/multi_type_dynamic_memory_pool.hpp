d560c000837af4c351b0a3421f0e19b33e4662a4
SpontanCombust <cedro_p@o2.pl>
2021-04-16 03:25:12 +0200

    Added basic memory pool class

---
+++ src/engine/memory/multi_type_dynamic_memory_pool.hpp
@@ -0,0 +1,41 @@
+#ifndef __CHESTNUT_MULTI_TYPE_FLEXIBLE_MEMORY_POOL_H__
+#define __CHESTNUT_MULTI_TYPE_FLEXIBLE_MEMORY_POOL_H__
+
+#include "engine/misc/tindex.hpp"
+
+#include <stack>
+#include <unordered_map>
+
+namespace chestnut
+{
+    class CMultiTypeDynamicMemoryPool
+    {
+    private:
+        std::unordered_map< std::type_index, std::stack< void * > > m_mapTypeToPtrStack;
+
+    public:
+        CMultiTypeDynamicMemoryPool() = default;
+
+        CMultiTypeDynamicMemoryPool( const CMultiTypeDynamicMemoryPool& ) = delete; // we don't copy the pointers
+
+        CMultiTypeDynamicMemoryPool( CMultiTypeDynamicMemoryPool&& other );
+
+        ~CMultiTypeDynamicMemoryPool();
+
+        // Return an allocated pointer of type T
+        // Transfers the ownership onto user
+        template< typename T >
+        T *create();
+
+        // Takes the ownership over pointer of type T
+        template< typename T >
+        void remove( T *ptr );
+    };
+    
+} // namespace chestnut
+
+
+#include "multi_type_dynamic_memory_pool.tpp"
+
+
+#endif // __CHESTNUT_MULTI_TYPE_FLEXIBLE_MEMORY_POOL_H__
