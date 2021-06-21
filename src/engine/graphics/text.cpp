b6035952b3adbb8db6609a850e25b58fc6dee742
SpontanCombust <cedro_p@o2.pl>
2021-06-21 03:35:48 +0200

    Added text class that uses font resource

---
+++ src/engine/graphics/text.cpp
@@ -0,0 +1,95 @@
+#include "text.hpp"
+
+#include <algorithm>
+
+namespace chestnut
+{    
+    CText::CText() 
+    {
+        m_pointSize = 0;
+        m_currentGlyphOffset = 0;
+    }
+
+    CText::CText( std::shared_ptr<CFontResource> fontResource, int pointSize ) 
+    {
+        m_resource = fontResource;
+        m_pointSize = pointSize;
+        m_currentGlyphOffset = 0;
+    }
+
+    bool CText::isValid() const
+    {
+        if( m_resource && m_resource->isValid() )
+        {
+            return true;
+        }
+        return false;
+    }
+
+    int CText::getPointSize() const
+    {
+        return m_pointSize;
+    }
+
+    void CText::clear() 
+    {
+        m_vecFragments.clear();
+        m_currentGlyphOffset = {0};
+    }
+
+    bool CText::isEmpty() const
+    {
+        return m_vecFragments.empty();
+    }
+
+    std::wstring CText::getString() const
+    {
+        std::wstring str;
+        for( const STextFragment& frag : m_vecFragments )
+        {
+            str += frag.str;
+        }
+
+        return str;
+    }
+
+    int CText::getLength() const
+    {
+        return getString().length();
+    }
+
+    void CText::appendString( std::wstring s, EFontStyle style, vec4f color ) 
+    {
+        const SFontConfig& config = m_resource->getConfig( m_pointSize, style );
+
+        STextFragment fragment;
+        fragment.str = s;
+        fragment.texID = config.glyphSpriteSheet.getID();
+        fragment.color = color;
+
+        for( wchar_t g : s )
+        {
+            const SGlyphMetrics& metrics = config.mapGlyphMetrics.at(g);
+            const SRectangle& clipRect = config.glyphSpriteSheet.getFragment( g );
+
+            STextGlyph glyph;
+            glyph.g = g;
+            glyph.posOffset = m_currentGlyphOffset + vec2f( metrics.xMin, 0 );
+            glyph.size = vec2f( metrics.width, metrics.height );
+            glyph.uvOffset = vec2f( clipRect.x, clipRect.y );
+            glyph.uvSize = vec2f( clipRect.w, clipRect.h );
+
+            fragment.vecGlyphs.push_back( glyph );
+            
+            m_currentGlyphOffset.x() += metrics.advance;
+        }
+
+        m_vecFragments.push_back( fragment );
+    }
+
+    const std::vector< STextFragment >& CText::getFragments() const
+    {
+        return m_vecFragments;
+    }
+
+} // namespace chestnut
---
+++ src/engine/graphics/text.hpp
@@ -0,0 +1,62 @@
+#ifndef __CHESTNUT_TEXT_H__
+#define __CHESTNUT_TEXT_H__
+
+#include "engine/maths/vector2.hpp"
+#include "engine/maths/vector4.hpp"
+#include "engine/resources/font_resource.hpp"
+
+#include <vector>
+
+namespace chestnut
+{
+    struct STextGlyph
+    {
+        wchar_t g;
+        vec2f posOffset;
+        vec2f size;
+        vec2f uvOffset;
+        vec2f uvSize;
+    };
+
+    struct STextFragment
+    {
+        std::wstring str; 
+        GLuint texID;
+        vec4f color;
+        std::vector< STextGlyph > vecGlyphs;
+    };
+
+    class CText
+    {
+    protected:
+        std::shared_ptr<CFontResource> m_resource;
+
+        int m_pointSize;
+        std::vector<STextFragment> m_vecFragments;
+
+        vec2f m_currentGlyphOffset;
+
+    public:
+        CText();
+        CText( std::shared_ptr<CFontResource> fontResource, int pointSize );
+
+        bool isValid() const;
+
+        int getPointSize() const;
+
+        void clear();
+
+        std::wstring getString() const;
+
+        bool isEmpty() const;
+        int getLength() const;
+
+        const std::vector< STextFragment >& getFragments() const;
+
+
+        virtual void appendString( std::wstring s, EFontStyle style = EFontStyle::NORMAL, vec4f color = { 1.f, 1.f, 1.f, 1.f } );
+    };
+
+} // namespace chestnut
+
+#endif // __CHESTNUT_TEXT_H__
