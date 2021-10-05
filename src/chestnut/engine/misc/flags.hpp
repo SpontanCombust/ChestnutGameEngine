8b6a74158bbc83e9a113e0d03be1f602755f9c19
SpontanCombust <cedro_p@o2.pl>
2021-10-05 09:37:54 +0200

    Added more flag operators

--- src/chestnut/engine/misc/flags.hpp
+++ src/chestnut/engine/misc/flags.hpp
@@ -2,48 +2,88 @@
 #define __CHESTNUT_ENGINE_FLAGS_H__
 
 #define DECLARE_ENUM_FLAG_OPERATORS(T) \
-    T operator|( const T& lhs, const T& rhs ); \
-    T& operator|=( T& lhs, const T& rhs ); \
-    T operator&( const T& lhs, const T& rhs ); \
-    T& operator&=( T& lhs, const T& rhs ); \
-    T operator^( const T& lhs, const T& rhs ); \
-    T& operator^=( T& lhs, const T& rhs ); \
-    T operator~( const T& rhs );
+    T operator|( T lhs, T rhs ); \
+    T& operator|=( T& lhs, T rhs ); \
+    T operator&( T lhs, T rhs ); \
+    T& operator&=( T& lhs, T rhs ); \
+    T operator^( T lhs, T rhs ); \
+    T& operator^=( T& lhs, T rhs ); \
+    T operator~( T rhs ); \
+    bool operator==( T lhs, int rhs ); \
+    bool operator==( int lhs, T rhs ); \
+    bool operator!=( T lhs, int rhs ); \
+    bool operator!=( int lhs, T rhs ); \
+    bool operator>( T lhs, int rhs ); \
+    bool operator>( int lhs, T rhs ); \
+    bool operator<( T lhs, int rhs ); \
+    bool operator<( int lhs, T rhs ); \
 
 #define DEFINE_ENUM_FLAG_OPERATORS(T) \
-    T operator|( const T& lhs, const T& rhs ) \
+    T operator|( T lhs, T rhs ) \
     { \
         return static_cast<T>( static_cast<int>(lhs) | static_cast<int>(rhs) ); \
     } \
-    T& operator|=( T& lhs, const T& rhs ) \
+    T& operator|=( T& lhs, T rhs ) \
     { \
         T result = lhs | rhs; \
         lhs = result; \
         return lhs; \
     } \
-    T operator&( const T& lhs, const T& rhs ) \
+    T operator&( T lhs, T rhs ) \
     { \
         return static_cast<T>( static_cast<int>(lhs) & static_cast<int>(rhs) ); \
     } \
-    T& operator&=( T& lhs, const T& rhs ) \
+    T& operator&=( T& lhs, T rhs ) \
     { \
         T result = lhs & rhs; \
         lhs = result; \
         return lhs; \
     } \
-    T operator^( const T& lhs, const T& rhs ) \
+    T operator^( T lhs, T rhs ) \
     { \
         return static_cast<T>( static_cast<int>(lhs) ^ static_cast<int>(rhs) ); \
     } \
-    T& operator^=( T& lhs, const T& rhs ) \
+    T& operator^=( T& lhs, T rhs ) \
     { \
         T result = lhs ^ rhs; \
         lhs = result; \
         return lhs; \
     } \
-    T operator~( const T& rhs ) \
+    T operator~( T rhs ) \
     { \
         return static_cast<T>( ~ static_cast<int>(rhs) ); \
-    }
+    } \
+    bool operator==( T lhs, int rhs ) \
+    { \
+        return static_cast<int>( lhs ) == rhs; \
+    } \
+    bool operator==( int lhs, T rhs ) \
+    { \
+        return static_cast<int>( rhs ) == lhs; \
+    } \
+    bool operator!=( T lhs, int rhs ) \
+    { \
+        return static_cast<int>( lhs ) != rhs; \
+    } \
+    bool operator!=( int lhs, T rhs ) \
+    { \
+        return static_cast<int>( rhs ) != lhs; \
+    } \
+    bool operator>( T lhs, int rhs ) \
+    { \
+        return static_cast<int>( lhs ) > rhs; \
+    } \
+    bool operator>( int lhs, T rhs ) \
+    { \
+        return lhs > static_cast<int>( rhs ); \
+    } \
+    bool operator<( T lhs, int rhs ) \
+    { \
+        return static_cast<int>( lhs ) < rhs; \
+    } \
+    bool operator<( int lhs, T rhs ) \
+    { \
+        return lhs < static_cast<int>( rhs ); \
+    } \
 
 #endif // __CHESTNUT_ENGINE_FLAGS_H__
