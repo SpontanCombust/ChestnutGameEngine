4bf30e15257f3aeee14e9f9b993b90b075250400
SpontanCombust <cedro_p@o2.pl>
2021-10-31 16:14:30 +0100

    TEST framebuffer

---
+++ tests/framebuffer_test.cpp
@@ -0,0 +1,75 @@
+#include <catch2/catch.hpp>
+
+#include "../src/chestnut/engine/init.hpp"
+#include "../src/chestnut/engine/main/window.hpp"
+#include "../src/chestnut/engine/graphics/text_renderer.hpp"
+#include "../src/chestnut/engine/graphics/sprite_renderer.hpp"
+
+#include "test_utils.hpp"
+
+
+using namespace chestnut::engine;
+
+TEST_CASE( "Renderers - Framebuffer test", "[interactive]" )
+{
+    const char *testName = "Renderers - Framebuffer test";
+
+    chestnutInit();
+
+    auto window = createWindow( testName );
+
+    CShaderProgram textShader( loadShaderProgramResourceFromFiles( "../assets/shaders/text.vert", "../assets/shaders/text.frag" ) );
+    CTextRenderer textRenderer;
+    REQUIRE_NOTHROW( textRenderer.init( textShader ) );
+    textRenderer.bindShader();
+    textRenderer.setViewMatrix( mat4f() );
+    textRenderer.setProjectionMatrix( matMakeOrthographic<float>( 0, window->getSizeWidth(), window->getSizeHeight(), 0, -1, 1 ) ); 
+
+    CShaderProgram spriteShader( loadShaderProgramResourceFromFiles( "../assets/shaders/sprite.vert", "../assets/shaders/sprite.frag" ) );
+    CSpriteRenderer spriteRenderer;
+    REQUIRE_NOTHROW( spriteRenderer.init( spriteShader ) );
+    spriteRenderer.bindShader();
+    spriteRenderer.setViewMatrix( mat4f() );
+    spriteRenderer.setProjectionMatrix( matMakeOrthographic<float>( 0, window->getSizeWidth(), window->getSizeHeight(), 0, -1, 1 ) ); 
+
+
+    showInfoMessageBox( testName, "This test should show the same results received through direct and indirect (through target texture) rendering of text." );
+
+
+    CText text( loadFontResourceFromFile( "../assets/fonts/arial.ttf" ), 32 );
+    text.append( L"This is an example text.\n" );
+    text.append( L"Remember how it looks!", EFontStyle::BOLD, vec3f{ 1.f, 0.f, 0.f } );
+    text.generateData();
+
+    window->clear();
+        textRenderer.bindShader();
+        textRenderer.clear();
+        textRenderer.submitText( text, vec2f{ 50.f, 50.f } );
+        textRenderer.render( window->getFramebuffer() );
+    window->flipBuffer();
+
+    showInfoMessageBox( testName, "This is a text rendered directly onto screen." );
+
+
+    CTexture2D blank = CTexture2D( loadTexture2DResourceFromPixels( nullptr, window->getSizeWidth(), window->getSizeHeight(), GL_RGBA, false ) );
+    CFramebuffer blankFramebuffer( blank );
+    blankFramebuffer.setClearColor( vec4f( 0.f ) ); 
+
+    window->clear();
+        blankFramebuffer.bind();
+        textRenderer.bindShader();
+        textRenderer.render( blankFramebuffer ); // retains the render data from previous pass
+
+        spriteRenderer.bindShader();
+        spriteRenderer.submitSprite( blank, vec2f( 0.f ) );
+        spriteRenderer.render( window->getFramebuffer() );
+    window->flipBuffer();
+
+    showInfoMessageBox( testName, "This is a text rendered first onto a sprite and then that sprite is rendered onto screen." );
+
+
+    REQUIRE( showConfirmMessageBox( testName ) );
+
+
+    chestnutQuit();
+}
