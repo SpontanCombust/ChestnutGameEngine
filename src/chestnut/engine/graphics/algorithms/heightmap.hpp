1272c79a00ec48dfb0b41ccf072c02d8166d903e
SpontanCombust <cedro_p@o2.pl>
2022-05-03 22:26:43 +0200

    feat: heightmap processing

---
+++ src/chestnut/engine/graphics/algorithms/heightmap.hpp
@@ -0,0 +1,28 @@
+#ifndef __CHESTNUT_ENGINE_HEIGHTMAP_H__
+#define __CHESTNUT_ENGINE_HEIGHTMAP_H__
+
+#include "../../resources/mesh_data_resource.hpp"
+
+#include <type_traits>
+
+
+namespace chestnut::engine
+{
+    struct SHeightmap
+    {
+        float *values; // height values
+        vec2i size; // size of the heightmap
+    };
+
+    // uses red channel to yield height values
+    // these values are then normalized to the range [0, 1] and scaled using valueScale
+    tl::expected<std::shared_ptr<SHeightmap>, const char *> heightmapFromFile(const char *path, float valueScale);
+
+    // heightmap - heightmap data
+    // dstSize - size in width and depth of the resulting mesh
+    // valueScale - scale applied to heightmap's values after they are clamped to srcMin and srcMax 
+    tl::expected<std::shared_ptr<CMeshDataResource>, const char *> meshDataFromHeightmap(const SHeightmap &heightmap, vec2f dstSize, float valueScale = 1.0f);
+    
+} // namespace chestnut::engine
+
+#endif // __CHESTNUT_ENGINE_HEIGHTMAP_H__
