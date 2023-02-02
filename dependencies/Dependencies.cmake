e4705141aa9da2e7e3bcc756bd97c76bb2a19d10
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2023-02-02 19:01:02 +0100

    reconfig output paths... again

--- dependencies/Dependencies.cmake
+++ dependencies/Dependencies.cmake
@@ -187,31 +187,3 @@ if(CHESTNUT_BUILD_TESTS)
     FetchContent_MakeAvailable(catch2)
     list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/contrib)
 endif()
-
-
-
-
-# ================================== UTILITIES =====================================
-
-macro(copy_shared_libs)
-    set(prefix P)
-    set(options OPTION)
-    set(singleValues TARGET)
-    set(multiValues DEPENDENCIES)
-
-    include(CMakeParseArguments)
-    cmake_parse_arguments(
-        "${prefix}"
-        "${options}"
-        "${singleValues}"
-        "${multiValues}"
-        "${ARGN}"
-    )
-
-    foreach(D IN ITEMS ${P_DEPENDENCIES})
-        add_custom_command(TARGET ${P_TARGET} POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:${D}>  $<TARGET_FILE_DIR:${P_TARGET}>
-        )
-    endforeach()
-    
-endmacro()
